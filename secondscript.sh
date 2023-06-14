ddd28df353d690cf687da5eeb203be5a8a449825
ZaydP <46539372+ZaydP@users.noreply.github.com>
2023-06-14 12:18:45 +0200

    Add files via upload

---
+++ secondscript.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --account=maths
+#SBATCH --partition=ada
+#BATCH --time=02:30:00
+#SBATCH --nodes=1 --ntasks=30
+#SBATCH --job-name="K-Complexity_test_script"
+#SBATCH --mail-user=pndzay001@myuct.ac.za
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+wolframscript -file K-Complexity_test_script.wls
